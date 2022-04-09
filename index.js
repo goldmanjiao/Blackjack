50c2acea7aca790f7d50fcb792275668cf935ebd
Goldman <goldmanjiao123456@gmail.com>
2022-04-10 00:38:46 +0100

    basic func

--- index.js
+++ index.js
@@ -3,4 +3,37 @@ let secondCard = 9
 
 let sum = firstCard + secondCard
 
-console.log(sum)
+let hasBlackJack = false
+let isAlive = true
+
+let message = ""
+let messageEl = document.getElementById("message-el")
+
+let sumEl = document.getElementById("sum-el")
+
+//let sumEl = document.querySelector("#sum-el")
+
+
+function startGame(){
+
+    sumEl.textContent = "Sum: " + sum
+
+    if (sum < 21){
+        message = "Do you want to draw a new card?"
+    } 
+    else if (sum == 21){
+        message = "Got blackjack!"
+        hasBlackJack = true
+    }
+    
+    else {
+        message = "Youre out of the game!"
+        isAlive = false
+    }
+
+    
+    messageEl.textContent = message 
+}
+
+
+
